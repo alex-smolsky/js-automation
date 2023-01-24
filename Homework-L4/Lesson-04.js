cf5a44aeda2f1ab786a22497cc4c214547dc298a
Alex Smolsky <alexsmolsky@Alexs-MacBook-Air.local>
2023-01-24 15:14:56 +0300

    node js initialized in project,fs-extra added to dependencies, methods for creating, moving, copying directories and files were added

---
+++ Homework-L4/Lesson-04.js
@@ -0,0 +1,26 @@
+const fs = require('fs-extra')
+
+fs.mkdirpSync("First_directory");
+
+fs.createFileSync("First_directory/FirstFile.txt");
+
+fs.mkdirpSync("Second_directory");
+
+fs.moveSync("First_directory/FirstFile.txt", "Second_directory/FirstFile.txt");
+
+fs.mkdirpSync("Third_directory");
+
+fs.copySync("Second_directory/FirstFile.txt", "Third_directory/FirstFile.txt");
+
+fs.removeSync("Second_directory/FirstFile.txt");
+
+fs.removeSync("Third_directory/FirstFile.txt");
+
+fs.removeSync("First_directory");
+
+fs.removeSync("Second_directory");
+
+fs.removeSync("Third_directory");
+
+
+
